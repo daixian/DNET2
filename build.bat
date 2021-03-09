44edb5a99efca6176712faa730d9d780fa58f62d
daixian <amano_tooko@qq.com>
2021-03-09 19:34:17 +0800

    加一个构建选项

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 chcp 65001
 REM windows平台编译项目
 set CONAN_REVISIONS_ENABLED=1
-conan create . daixian/stable -s compiler.runtime=MD -s arch=x86_64 -s build_type=Release --build missing
+conan create . daixian/stable -s compiler.runtime=MD -s arch=x86_64 -s build_type=Release -o poco:enable_netssl=False --build missing
 REM conan upload DNET/1.0.0@daixian/stable --all -r=conan-local
